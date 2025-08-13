97c3bb3612139f632db0efca10ffb44593d45ac0
Lucas Bardeli <lucasbbsjau@gmail.com>
2025-08-12 21:46:31 -0300

    Segunda aula. Primeiros passos na construção da API

--- server.ts
+++ server.ts
@@ -1,70 +1,46 @@
-
-// JavaScript
-// const fastify = require('fastify')
-// const crypto = require('crypto')
-
-import fastify from "fastify"
-import crypto from "node:crypto"
+import fastify from 'fastify'
+import { fastifySwagger } from '@fastify/swagger'
+import { validatorCompiler, serializerCompiler, type ZodTypeProvider, jsonSchemaTransform } from 'fastify-type-provider-zod'
+import { createCourseRoute } from './src/routes/create-course.ts'
+import { getCourseByIdRoute } from './src/routes/get-course-by-id.ts'
+import { getCoursesRoute } from './src/routes/get-courses.ts'
+import scalarAPIReference from '@scalar/fastify-api-reference'
 
 const server = fastify({
-    logger: {
-        transport: {
-            target: 'pino-pretty',
-            options: {
-                translateTime: 'HH:MM:ss Z',
-                ignore: 'pid,hostname',
-            },
-        },
-    }
-})
+  logger: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname',
+      },
+    },
+  },
+}).withTypeProvider<ZodTypeProvider>()
 
-const courses = [
-    { id: '1', title: 'Curso de Node.js'},
-    { id: '2', title: 'Curso de React'},
-    { id: '3', title: 'Curso de React Native'}
-]
+if (process.env.NODE_ENV === 'development') {
+  server.register(fastifySwagger, {
+    openapi: {
+      info: {
+        title: 'Desafio Node.js',
+        version: '1.0.0',
+      }
+    },
+    transform: jsonSchemaTransform,
+  })
+  
+  server.register(scalarAPIReference, {
+    routePrefix: '/docs',
+  })
+}
 
-server.get('/courses', (request, reply) => {
-    return reply.send({ courses })
-})
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
 
-server.get('/courses/:id', (request, reply) => {
-    type Params = {
-        id: string
-    }
-
-    const params = request.params as Params
-    const courseId = params.id
-    const course = courses.find(course => course.id === courseId)
-
-    if (course) {
-        return { course }
-    }
-
-    // 404 => Not found
-    return reply.status(404).send()
-})
-
-server.post('/courses', (request, reply) => {
-    type Body = {
-        title: string
-    }
-    
-    const body = request.body as Body
-    const courseId = crypto.randomUUID()
-    const courseTitle = body.title
-
-    if (!courseTitle) {
-        return reply.status(400).send({ message: 'Título obrigatório' })
-    }
-
-    courses.push({ id: courseId, title: courseTitle })
-
-    return reply.status(201).send({ courseId })
-})
-
-// Sempre retorne um objeto nas suas rotas! 
+server.register(createCourseRoute)
+server.register(getCourseByIdRoute)
+server.register(getCoursesRoute)
 
 server.listen({ port: 3333 }).then(() => {
-    console.log('HTTP server running!')
+  console.log('HTTP server running!')
 })
